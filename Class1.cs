20901dbaecc51b725840321d8f4608038eb2a95d
Your Name <you@example.com>
2022-03-30 15:43:54 +0530

    added hello program

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swiss
+{
+    public class Class1
+
+    {
+        public static void main()
+        {
+            System.Console.WriteLine("vandana");
+        }
+    }
+}
